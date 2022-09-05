17e3d79deb20351f0b486c9e6eba1c55b8a1265f
Dario Spina <dario.spina88@gmail.com>
2022-09-05 18:31:32 -0300

    proyecto elije tu entrada

--- app.js
+++ app.js
@@ -1,50 +1,251 @@
-let zona = " ";
+tarjetaDatos = "";
+total = 0;
 
+function tipoEntradas(){
+    let listaEntradas = Number(prompt(`Que entrada desea adquirir:
+    1 - Recital de Cumbia / campo $4000
+    2 - Recital de Electronica / campo $5000
+    3 - Recital de Rock / campo $4500
+    4 - Recital de Reggae / campo $3500
+    5 - Recital de Reggaeton / campo $3000`));
+    let entrada = 0;1
+    let precio = 0;
+    let tarjeta = 0;
+    let tranferencia = 0;
+    let confirmar = "";
 
-while (zona != "Esc" && zona != "esc") {
+    switch(listaEntradas){
 
-    let bienvenida = `¿Qué País de latinoamerica eres?, 
-La primera letra con MAYUSCULA`;
-        alert(bienvenida);
+        case 1 :
+            alert("Comprar entrada recital de cumbia");
+            entrada = Number(prompt("Total de entradas"));
+            precio = 4000;
+            total = totalPago(entrada, precio);
+            alert(`Monto total a abonar $${total}`);
+            confirmar = Number(prompt("Metodo de pago \n1 - Tarjeta \n2 - Tranferencia \n3 - Salir"));
 
-    zona = prompt("De que País eres? La primera letra con MAYUSCULA",
-        "Ingrese ESC o esc para interrumpir");
+            if (confirmar == 1){
+                alert("Datos de la tarjeta");
+                numeroTarjeta = Number(prompt("Numero de su Tarjeta"));
+                nombreTarjeta = prompt("Nombre y Apellido de su tarjeta");
+                tarjetaDatos = datosTarjeta(`\n${numeroTarjeta}, \n${nombreTarjeta}`);
+                alert(`Su tarjeta es ${tarjetaDatos}`);
+                importe = Number(prompt(`El importe es $${total}`, "Ingrese el importe ¡AQUI!"));
+                cuotas = Number(prompt("Numero de cuotas 3 / 6"));
+                confirmar = Number(prompt("1 pagar \n2 no pagar"));
+                if(confirmar == 1){
+                    alert(`Se concreto su compra!! $${total}`);
+                    alert("Felicidades ¡¡¡Compro su entrada!!! espere el mail con su entrada, saludos.");
+                } 
+                else {
+                    alert(`Cancelo la compra!! $${total}`);
+                }
+            } 
+            else if(confirmar == 2){
+                alert("Usted selecciono tranferencia");
+                tranferencia = Number(prompt(`El monto de la tranferencia es $${total}`, "Ingrese el Importe ¡AQUI!"));
+                confirmar = Number(prompt("1 pagar \n2 no pagar"));
 
-        alert("El usuario ingresó "+ zona);
-    
-    switch (zona) {
-        case "Argentina":
-            console.log(`País: ${zona}, 45.808.747 habitantes`);
+                if (confirmar == 1){
+                    alert(`Se concreto su compra!! $${total}`);
+                    alert("Felicidades ¡¡¡Compro su entrada!!! espere el mail con su entrada, saludos.");
+                } 
+                else {
+                    alert(`Cancelo la compra!! $${total}`);
+                }
+            }
+            else{
+                alert("Usted ha salido.");
+            }
             break;
-        case "Brasil":
-            console.log(`País: ${zona}, 213.993.441 habitantes`);
+
+            case 2 :
+            alert("Comprar entrada recital de Electronica");
+            entrada = Number(prompt("Total de entradas"));
+            precio = 5000;
+            total = totalPago(entrada, precio);
+            alert(`Monto total a abonar $${total}`);
+            confirmar = Number(prompt("Metodo de pago \n1 - Tarjeta \n2 - Tranferencia \n3 - Salir"));
+
+            if (confirmar == 1){
+                alert("Datos de la tarjeta");
+                numeroTarjeta = Number(prompt("Numero de su Tarjeta"));
+                nombreTarjeta = prompt("Nombre y Apellido de su tarjeta");
+                tarjetaDatos = datosTarjeta(`\n${numeroTarjeta}, \n${nombreTarjeta}`);
+                alert(`Su tarjeta es ${tarjetaDatos}`);
+                importe = Number(prompt(`El importe es $${total}`, "Ingrese el importe ¡AQUI!"));
+                cuotas = Number(prompt("Numero de cuotas 3 / 6"));
+                confirmar = Number(prompt("1 pagar \n2 no pagar"));
+                if(confirmar == 1){
+                    alert(`Se concreto su compra!! $${total}`);
+                    alert("Felicidades ¡¡¡Compro su entrada!!! espere el mail con su entrada, saludos.");
+                } 
+                else {
+                    alert(`Cancelo la compra!! $${total}`);
+                }
+            } 
+            else if(confirmar == 2){
+                alert("Usted selecciono tranferencia");
+                tranferencia = Number(prompt(`El monto de la tranferencia es $${total}`, "Ingrese el Importe ¡AQUI!"));
+                confirmar = Number(prompt("1 pagar \n2 no pagar"));
+
+                if (confirmar == 1){
+                    alert(`Se concreto su compra!! $${total}`);
+                    alert("Felicidades ¡¡¡Compro su entrada!!! espere el mail con su entrada, saludos.");
+                } 
+                else {
+                    alert(`Cancelo la compra!! $${total}`);
+                }
+            }
+            else{
+                alert("Usted ha salido.");
+            }
             break;
-        case "Chile":
-            console.log(`País: ${zona}, 19.212.362 habitantes`);
+
+            case 3 :
+            alert("Comprar entrada recital de Rock");
+            entrada = Number(prompt("Total de entradas"));
+            precio = 4500;
+            total = totalPago(entrada, precio);
+            alert(`Monto total a abonar $${total}`);
+            confirmar = Number(prompt("Metodo de pago \n1 - Tarjeta \n2 - Tranferencia \n3 - Salir"));
+
+            if (confirmar == 1){
+                alert("Datos de la tarjeta");
+                numeroTarjeta = Number(prompt("Numero de su Tarjeta"));
+                nombreTarjeta = prompt("Nombre y Apellido de su tarjeta");
+                tarjetaDatos = datosTarjeta(`\n${numeroTarjeta}, \n${nombreTarjeta}`);
+                alert(`Su tarjeta es ${tarjetaDatos}`);
+                importe = Number(prompt(`El importe es $${total}`, "Ingrese el importe ¡AQUI!"));
+                cuotas = Number(prompt("Numero de cuotas 3 / 6"));
+                confirmar = Number(prompt("1 pagar \n2 no pagar"));
+                if(confirmar == 1){
+                    alert(`Se concreto su compra!! $${total}`);
+                    alert("Felicidades ¡¡¡Compro su entrada!!! espere el mail con su entrada, saludos.");
+                } 
+                else {
+                    alert(`Cancelo la compra!! $${total}`);
+                }
+            } 
+            else if(confirmar == 2){
+                alert("Usted selecciono tranferencia");
+                tranferencia = Number(prompt(`El monto de la tranferencia es $${total}`, "Ingrese el Importe ¡AQUI!"));
+                confirmar = Number(prompt("1 pagar \n2 no pagar"));
+
+                if (confirmar == 1){
+                    alert(`Se concreto su compra!! $${total}`);
+                    alert("Felicidades ¡¡¡Compro su entrada!!! espere el mail con su entrada, saludos.");
+                } 
+                else {
+                    alert(`Cancelo la compra!! $${total}`);
+                }
+            }
+            else{
+                alert("Usted ha salido.");
+            }
             break;
-        case "Uruguay":
-            console.log(`País: ${zona}, 3.485.152 habitantes`);
+
+            case 4 :
+            alert("Comprar entrada recital de Reggae");
+            entrada = Number(prompt("Total de entradas"));
+            precio = 3500;
+            total = totalPago(entrada, precio);
+            alert(`Monto total a abonar $${total}`);
+            confirmar = Number(prompt("Metodo de pago \n1 - Tarjeta \n2 - Tranferencia \n3 - Salir"));
+
+            if (confirmar == 1){
+                alert("Datos de la tarjeta");
+                numeroTarjeta = Number(prompt("Numero de su Tarjeta"));
+                nombreTarjeta = prompt("Nombre y Apellido de su tarjeta");
+                tarjetaDatos = datosTarjeta(`\n${numeroTarjeta}, \n${nombreTarjeta}`);
+                alert(`Su tarjeta es ${tarjetaDatos}`);
+                importe = Number(prompt(`El importe es $${total}`, "Ingrese el importe ¡AQUI!"));
+                cuotas = Number(prompt("Numero de cuotas 3 / 6"));
+                confirmar = Number(prompt("1 pagar \n2 no pagar"));
+                if(confirmar == 1){
+                    alert(`Se concreto su compra!! $${total}`);
+                    alert("Felicidades ¡¡¡Compro su entrada!!! espere el mail con su entrada, saludos.");
+                } 
+                else {
+                    alert(`Cancelo la compra!! $${total}`);
+                }
+            } 
+            else if(confirmar == 2){
+                alert("Usted selecciono tranferencia");
+                tranferencia = Number(prompt(`El monto de la tranferencia es $${total}`, "Ingrese el Importe ¡AQUI!"));
+                confirmar = Number(prompt("1 pagar \n2 no pagar"));
+
+                if (confirmar == 1){
+                    alert(`Se concreto su compra!! $${total}`);
+                    alert("Felicidades ¡¡¡Compro su entrada!!! espere el mail con su entrada, saludos.");
+                } 
+                else {
+                    alert(`Cancelo la compra!! $${total}`);
+                }
+            }
+            else{
+                alert("Usted ha salido.");
+            }
             break;
-        case "Peu":
-            console.log(`País: ${zona}, 7.453.695 habitantes`);
+
+            case 5 :
+            alert("Comprar entrada recital de Reggaeton");
+            entrada = Number(prompt("Total de entradas"));
+            precio = 3000;
+            total = totalPago(entrada, precio);
+            alert(`Monto total a abonar $${total}`);
+            confirmar = Number(prompt("Metodo de pago \n1 - Tarjeta \n2 - Tranferencia \n3 - Salir"));
+
+            if (confirmar == 1){
+                alert("Datos de la tarjeta");
+                numeroTarjeta = Number(prompt("Numero de su Tarjeta"));
+                nombreTarjeta = prompt("Nombre y Apellido de su tarjeta");
+                tarjetaDatos = datosTarjeta(`\n${numeroTarjeta}, \n${nombreTarjeta}`);
+                alert(`Su tarjeta es ${tarjetaDatos}`);
+                importe = Number(prompt(`El importe es $${total}`, "Ingrese el importe ¡AQUI!"));
+                cuotas = Number(prompt("Numero de cuotas 3 / 6"));
+                confirmar = Number(prompt("1 pagar \n2 no pagar"));
+                if(confirmar == 1){
+                    alert(`Se concreto su compra!! $${total}`);
+                    alert("Felicidades ¡¡¡Compro su entrada!!! espere el mail con su entrada, saludos.");
+                } 
+                else {
+                    alert(`Cancelo la compra!! $${total}`);
+                }
+            } 
+            else if(confirmar == 2){
+                alert("Usted selecciono tranferencia");
+                tranferencia = Number(prompt(`El monto de la tranferencia es $${total}`, "Ingrese el Importe ¡AQUI!"));
+                confirmar = Number(prompt("1 pagar \n2 no pagar"));
+
+                if (confirmar == 1){
+                    alert(`Se concreto su compra!! $${total}`);
+                    alert("Felicidades ¡¡¡Compro su entrada!!! espere el mail con su entrada, saludos.");
+                } 
+                else {
+                    alert(`Cancelo la compra!! $${total}`);
+                }
+            }
+            else{
+                alert("Usted ha salido.");
+            }
             break;
-        case "Bolivia":
-            console.log(`País: ${zona}, 11.832.936 habitantes`);
-            break;
-        case "Colombia":
-            console.log(`País: ${zona}, 51,6 millones de habitantes`);
-        break;
-        case "Venezuela":
-            console.log(`País: ${zona}, 28.704.947 habitantes`);
-            break;
-        case "Mexico":
-            console.log(`País: ${zona}, 130.262.220 habitantes`);
-            break;
-        case "Ecuador":
-            console.log(`País: ${zona}, 18.000.000 habitantes`);
-            break;
-        default:
-            console.log("No ingreso ningun País");
-            break;
+    }
 }
+
+tipoEntradas();
+
+function datosTarjeta(numeroTarjeta, nombreTarjeta){
+    tarjeta = numeroTarjeta + nombreTarjeta
+    return tarjeta;
+}
+
+function pago(importe, cuotas){
+    resultado = importe / cuotas;
+    return resultado
+}
+
+function totalPago(entrada,precio){
+    total = entrada * precio
+    return total;
 }
