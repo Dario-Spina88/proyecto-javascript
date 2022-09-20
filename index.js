41bdde0272a1b86f0e34df481ed72342baea655d
Dario Spina <dario.spina88@gmail.com>
2022-09-19 22:53:46 -0300

    Se agrego imagen a las cards

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const array = [];
+let saludar = alert("Hola!! Bienvenido a Ticket-In");
+let usuario = prompt("Ingrese su usuario");
+let contrasenia = prompt("Ingrese su contraseña");
+
+array.push(saludar);
+array.push(usuario);
+array.push(contrasenia);
+
+function contUsuario(){
+    if((usuario == "dario") && (contrasenia == 123)){
+    alert("Bienvenido "+usuario);
+} else {
+    alert("No ingreso bien el usuario o la contraseña");
+}
+}
+contUsuario();
+
 
 class Espectaculo {
     constructor(id, evento, detalle, precio, img, cantidad) {
@@ -11,11 +29,11 @@ class Espectaculo {
 }
 
 let infoEntrada = [];
-infoEntrada.push(new Espectaculo("1", "Teatro", "Para Niño/a", 4000, "src/img/niños.jpg", 2))
-infoEntrada.push(new Espectaculo("2", "Teatro", "Para Adulto/a", 5000, "src/img/adultos.jpg", 3))
-infoEntrada.push(new Espectaculo("3", "Recital", "Electronica", 6000, "src/img/niños.jpg", 2))
-infoEntrada.push(new Espectaculo("4", "Recital", "Rock", 4500, "src/img/niños.jpg", 4))
-infoEntrada.push(new Espectaculo("5", "Recital", "Reggae", 3000, "src/img/niños.jpg", 6))
+infoEntrada.push(new Espectaculo("1", "Teatro", "Para Niño/a", 4000, "/img/niños.jpg", 2))
+infoEntrada.push(new Espectaculo("2", "Teatro", "Para Adulto/a", 5000, "/img/adultos.jpg", 3))
+infoEntrada.push(new Espectaculo("3", "Festival", "Electronica", 6000, "/img/electronica.jpg", 2))
+infoEntrada.push(new Espectaculo("4", "Recital", "Rock", 4500, "/img/rock.jpg", 4))
+infoEntrada.push(new Espectaculo("5", "Recital", "Reggae", 3000, "/img/reggae.jpg", 6))
 
 
 //-----------------DOM----------------
@@ -31,7 +49,10 @@ infoEntrada.forEach((espectaculo)=> {
     let cardClonada = card.cloneNode(true)
     section.appendChild(cardClonada)
 
-    cardClonada.children[0].innerHTML = espectaculo.img
+// imagen------------------
+    let img = cardClonada.querySelector("img")
+    img.src = espectaculo.img
+
     cardClonada.children[1].innerText = espectaculo.evento
     cardClonada.children[2].innerText = espectaculo.detalle
     cardClonada.children[3].innerText = "C/ Entrada $"+ espectaculo.precio
