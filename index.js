61fdac0fdfbffab2959ea50ba06e07d6aa8d750f
Dario Spina <dario.spina88@gmail.com>
2022-10-17 18:13:34 -0300

    Modificacion de index.js

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    dataFetch();
-})
-
-
+let carrito = [];
+const compraCarroEntradasContainer = document.querySelector(
+  ".compraCarroEntradasContainer"
+);
 
 let section = document.getElementById("tarjeta");
 
 let infoEntradas = [];
 
-async function dataFetch () {
-    const resp = await fetch("/data.json");
-    const data = await resp.json();
-    infoEntradas = data;
-    listaEntradas()
+async function dataFetch() {
+  const resp = await fetch("/data.json");
+  const data = await resp.json();
+  infoEntradas = data;
+  listaEntradas();
 }
 
 function listaEntradas() {
-    infoEntradas.forEach((entrada)=>{
-        let cardEntrada = document.createElement("div")
-        cardEntrada.innerHTML += `
+  infoEntradas.forEach((entrada) => {
+    let cardEntrada = document.createElement("div");
+    cardEntrada.innerHTML += `
                     <div>
                         <article class="card h-100">
                             <img src="${entrada.img}" class="card-img" alt="" />
@@ -27,81 +26,45 @@ function listaEntradas() {
                             <button data-id="${entrada.id}" id="comprar${entrada.id}" class="btn btn-primary" type="button">AÑADIR AL CARRITO</button>
                         </article>
                     </div>
-                `
-                section.appendChild(cardEntrada)
-    })
+                `;
+    section.appendChild(cardEntrada);
+
+    cardEntrada.querySelector(".btn").addEventListener("click", () => {
+      existeid = carrito.findIndex((e) => e.id === entrada.id);
+      if (existeid == -1) {
+        carrito.push({
+          id: entrada.id,
+          titulo: entrada.titulo,
+          precio: entrada.precio,
+          img: entrada.img,
+        });
+        localStorage.setItem("carrito_daro", JSON.stringify(carrito));
+        carritoDeCompras();
+      }
+    });
+  });
 }
-
-
-
-let carrito = JSON.parse(localStorage.getItem("carrito")) || []
-
-
-//-------------Boton agregar al carrito------------------
-
-const compraDeEntradas = document.querySelectorAll("#comprar");
-compraDeEntradas.forEach((botonDeCompra) => {
-    botonDeCompra.addEventListener("click", agregarAlCarrito);
-});
 
 
 // -----------------Boton para comprar----------------
 
 const botonDeCompra = document.querySelector(".botonDeCompra");
-botonDeCompra.addEventListener("click",botonDeCompraClicked);
+botonDeCompra.addEventListener("click", botonDeCompraClicked);
 
 // -----------------Boton para Vaciar----------------
 
 const botonDeVaciar = document.querySelector(".botonDeVaciar");
-botonDeVaciar.addEventListener("click",botonDeVaciarClicked);
+botonDeVaciar.addEventListener("click", botonDeVaciarClicked);
 
 
-const compraCarroEntradasContainer = document.querySelector(
-    ".compraCarroEntradasContainer"
-);
+
 
 
 //------------------Agregar al carrito-------------
 
-function agregarAlCarrito(e) {
-
-    const button = e.target;
-
-    const card = button.closest(".card");
-
-    const cardTitulo = card.querySelector(".card-titulo").textContent;
-    const cardPrecio = card.querySelector(".card-precio").textContent;
-    
-    const cardImagen = card.querySelector(".card-img").src;
-
-    carritoDeCompras(cardTitulo, cardPrecio, cardImagen);
-
-// ---------localStorage----------
-const compraCarroEntradas = entradaCarritoGet()
-agregarLocalStorage("entrada", compraCarroEntradas)
-// ------------------------------------
-
-}
-
-
-
-
-// function carritoDeCompras(cardTitulo, cardPrecio, cardImagen) {
-
-//     const soloUnTitulo = compraCarroEntradasContainer.getElementsByClassName("compraCarroEntradaTitulo");
-    
-//     for(let i = 0; i < soloUnTitulo.length; i++){
-//         if (soloUnTitulo[i].innerText === cardTitulo){
-
-//             const cantidadEntrada = soloUnTitulo[i].parentElement.parentElement.parentElement.querySelector(".compraCarroEntradaCantidad");
-//             cantidadEntrada.value++;
-//             compraTotal();
-//             return;
-//         }    
-//     }
-
-function carritoDeCompras(entrada){
-
+function carritoDeCompras() {
+  compraCarroEntradasContainer.textContent = "";
+  carrito.forEach((entrada) => {
     const filaCompraContenedor = document.createElement("div");
     const compraCarroContenedor = `
     <div class="row compraCarroEntrada">
@@ -113,7 +76,7 @@ function carritoDeCompras(entrada){
         </div>
         <div class="col-2">
             <div class="compra-carro-precio d-flex align-items-center h-50 border-bottom pb-2 pt-3">
-                <p class="card-precio mb-0 compraCarroEntradaPrecio">${entrada.precio}</p>
+                <p class="card-precio mb-0 compraCarroEntradaPrecio">$${entrada.precio}</p>
             </div>
         </div>
         <div class="col-4">
@@ -126,109 +89,125 @@ function carritoDeCompras(entrada){
     filaCompraContenedor.innerHTML = compraCarroContenedor;
     compraCarroEntradasContainer.appendChild(filaCompraContenedor);
 
-
     // ----------boton Eliminar-------------
-    filaCompraContenedor.querySelector(".buttonDelete").addEventListener("click",removeCompraDeEntrada);
+    filaCompraContenedor
+      .querySelector(".buttonDelete")
+      .addEventListener("click", removeCompraDeEntrada);
 
     //----------Flecha para subir y bajar cantidad--------------
-    filaCompraContenedor.querySelector(".compraCarroEntradaCantidad").addEventListener("change", cambiarCantidad);
+    filaCompraContenedor
+      .querySelector(".compraCarroEntradaCantidad")
+      .addEventListener("change", cambiarCantidad);
 
     compraTotal();
+  });
 }
-
 
 // -----------Compra de Carrito----------------
 
-function compraTotal(){
-    let total = 0;
-    const compraDelTotal = document.querySelector(".compraDelTotal")
-    
-    const compraCarroEntradas = document.querySelectorAll(".compraCarroEntrada");
-    
-    compraCarroEntradas.forEach(compraCarroEntrada =>{
-        const compraCarroEntradaPrecioElement = compraCarroEntrada.querySelector(".compraCarroEntradaPrecio");
+function compraTotal() {
+  let total = 0;
+  const compraDelTotal = document.querySelector(".compraDelTotal");
 
-        const compraCarroEntradaPrecio = Number(compraCarroEntradaPrecioElement.textContent.replace("$",""));
+  const compraCarroEntradas = document.querySelectorAll(".compraCarroEntrada");
 
-        const compraCarroEntradaCantidadElement = compraCarroEntrada.querySelector(".compraCarroEntradaCantidad");
+  compraCarroEntradas.forEach((compraCarroEntrada) => {
+    const compraCarroEntradaPrecioElement = compraCarroEntrada.querySelector(
+      ".compraCarroEntradaPrecio"
+    );
 
-        
-        const compraCarroEntradaCantidad = Number(compraCarroEntradaCantidadElement.value);
-        
-        total = total + compraCarroEntradaPrecio * compraCarroEntradaCantidad;
-    });
+    const compraCarroEntradaPrecio = Number(
+      compraCarroEntradaPrecioElement.textContent.replace("$", "")
+    );
 
-    compraDelTotal.innerHTML = `$${total}`
-    
+    const compraCarroEntradaCantidadElement = compraCarroEntrada.querySelector(
+      ".compraCarroEntradaCantidad"
+    );
+
+    const compraCarroEntradaCantidad = Number(
+      compraCarroEntradaCantidadElement.value
+    );
+
+    total = total + compraCarroEntradaPrecio * compraCarroEntradaCantidad;
+  });
+
+  compraDelTotal.innerHTML = `$${total}`;
 }
 
+function removeCompraDeEntrada(e) {
+  const botonClicked = e.target;
 
-function removeCompraDeEntrada(e){
-    const botonClicked = e.target;
-    
-    botonClicked.closest(".compraCarroEntrada").remove();
-    compraTotal();
+  botonClicked.closest(".compraCarroEntrada").remove();
+  compraTotal();
 }
-
 
 //----------Flecha para subir y bajar cantidad y ternario--------------
 
-function cambiarCantidad(e){
-    const input = e.target;
-    input.value <= 0 ? (input.value = 1) : null;
-    compraTotal();    
+function cambiarCantidad(e) {
+  const input = e.target;
+  input.value <= 0 ? (input.value = 1) : null;
+  compraTotal();
 }
 
+function botonDeVaciarClicked() {
+  compraCarroEntradasContainer.innerHTML = "";
+  carrito = [];
+  compraTotal();
 
-function botonDeVaciarClicked(){
-    compraCarroEntradasContainer.innerHTML = "";
-    compraTotal();
-
-    Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Cancelo la compra',
-    })
+  Swal.fire({
+    icon: "error",
+    title: "Oops...",
+    text: "Cancelo la compra",
+  });
 }
 
+function botonDeCompraClicked() {
+  compraCarroEntradasContainer.innerHTML = "";
+  carrito = [];
+  compraTotal();
 
-function botonDeCompraClicked(){
-
-    compraCarroEntradasContainer.innerHTML = "";
-    compraTotal();
-
-    Swal.fire({
-        position: 'center',
-        icon: 'success',
-        title: 'Compro su entrada',
-        text: 'Espere email con las entradas',
-        showConfirmButton: false,
-        timer: 1500
-    })
+  Swal.fire({
+    position: "center",
+    icon: "success",
+    title: "Compro su entrada",
+    text: "Espere email con las entradas",
+    showConfirmButton: false,
+    timer: 1500,
+  });
 }
 
+function entradaCarritoGet() {
+  const compraCarroEntradas = document.querySelectorAll(".compraCarroEntrada");
+  const arrayEntrada = [];
 
-function entradaCarritoGet (){
-    const compraCarroEntradas = document.querySelectorAll(".compraCarroEntrada");
-    const arrayEntrada = [];
+  compraCarroEntradas.forEach((compraCarroEntrada) => {
+    const compraCarroEntradaTituloElement = compraCarroEntrada.querySelector(
+      ".compraCarroEntradaTitulo"
+    );
+    const compraCarroEntradaTitulo =
+      compraCarroEntradaTituloElement.textContent;
 
-    compraCarroEntradas.forEach(compraCarroEntrada =>{
-        
-        const compraCarroEntradaTituloElement = compraCarroEntrada.querySelector(".compraCarroEntradaTitulo")
-        const compraCarroEntradaTitulo = compraCarroEntradaTituloElement.textContent
+    const compraCarroEntradaCantidadElement = compraCarroEntrada.querySelector(
+      ".compraCarroEntradaCantidad"
+    );
+    const compraCarroEntradaCantidad = Number(
+      compraCarroEntradaCantidadElement.value
+    );
 
-        const compraCarroEntradaCantidadElement = compraCarroEntrada.querySelector(".compraCarroEntradaCantidad")
-        const compraCarroEntradaCantidad = Number(compraCarroEntradaCantidadElement.value)
+    arrayEntrada.push(compraCarroEntradaTitulo);
+    arrayEntrada.push(compraCarroEntradaCantidad);
+  });
 
-
-        arrayEntrada.push(compraCarroEntradaTitulo)
-        arrayEntrada.push(compraCarroEntradaCantidad)
-    })
-
-    return arrayEntrada;
+  return arrayEntrada;
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  dataFetch();
 
-function agregarLocalStorage(key, cardTitulo){
-    localStorage.setItem(key, JSON.stringify(cardTitulo))
-}
+  if (localStorage.getItem("carrito_daro")) {
+    carrito = JSON.parse(localStorage.getItem("carrito_daro"));
+    carritoDeCompras();
+  } else {
+    carrito = [];
+  }
+});
